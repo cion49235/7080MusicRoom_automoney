63facab9b9a9850dfc2b2e19854269ad565e863c
cion49235 <cion49235@nate.com>
2018-03-27 14:01:03 +0900

    [20180327]7080 별밤음악

---
+++ src/com/oldmusic/room/automoney/data/SubCategory_Data.java
@@ -0,0 +1,12 @@
+package com.oldmusic.room.automoney.data;
+
+
+public class SubCategory_Data {
+	 public String id;
+	 public String num;
+	 public String title;
+	 public String provider;
+	 public String imageurl;
+	 public String rssurl;
+	 public String udate;
+ }
---
+++ src/com/oldmusic/room/automoney/data/Sub_Data.java
@@ -0,0 +1,11 @@
+package com.oldmusic.room.automoney.data;
+
+
+public class Sub_Data {
+	 public String title;
+	 public String provider;
+	 public String enclosure;
+	 public String pubDate;
+	 public String image;
+	 public String description_title;
+ }
